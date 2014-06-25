9ea66ac1ca899264199d34d475d2907ea2169657
Camille Tjhoa <ctjhoa@linagora.com>
2014-06-25 11:23:27 +0200

    Add first submit / reset functions

--- app/states/upload_request/upload_request-controller.js
+++ app/states/upload_request/upload_request-controller.js
@@ -5,14 +5,53 @@ goog.provide('my.upload_request.Ctrl');
 /**
  * UploadRequest controller.
  *
+ * @param {!angular.$http} $http The angular http service
+ * @param {!my.app.lsAppConfig} lsAppConfig The linshare configuration
  * @constructor
+ * @ngInject
+ * @export
  */
-my.upload_request.Ctrl = function() {
+my.upload_request.Ctrl = function($http, lsAppConfig) {
 
   /**
-   * @type {Array}
+   * @type {!angular.http}
+   */
+  this.http_ = $http;
+
+  /**
+   * @type {!my.app.lsAppConfig}
+   */
+  this.lsAppConfig_ = lsAppConfig;
+
+  /**
+   * @type {Object}
    * @expose
    */
-  this.animals = ['dog', 'cat', 'mouse'];
+  this.form = {};
+};
 
+/**
+ * Submit the form
+ *
+ * @export
+ */
+my.upload_request.Ctrl.prototype.submit = function() {
+  var http = this.http_;
+  var lsAppConfig = this.lsAppConfig_;
+  var form = this.form;
+
+  console.debug('SUBMIT');
+  http.post(lsAppConfig.backendURL + '/upload_request', form);
+};
+
+/**
+ * Reset the form
+ *
+ * @export
+ */
+my.upload_request.Ctrl.prototype.reset = function() {
+  var form = this.form;
+
+  console.debug('RESET');
+  form = {};
 };
